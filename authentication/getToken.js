12995ae8fdd5d53eed0c36aa90611a4ef164b2b0
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-04-11 22:13:00 -0400

    extends token duration to 24h

--- authentication/getToken.js
+++ authentication/getToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (username, user_id) => {
     const payload = {username, user_id}
     const secret = process.env.JWT_SECRET || 'this is super secret, crack it if you dare';
-    const options = { expiresIn: '5hr'}
+    const options = { expiresIn: '24hr'}
 
     return jwt.sign(payload, secret, options)
 }
